5b45efdc88e09c0f7c63b62adffea9da23320e06
David Rojas <davidsrojasll@gmail.com>
2026-07-19 10:45:20 -0500

    Giphy API Servicio de Gifs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: true,
     companyName: 'Gifs',
     companyName2: 'App',
-    CompanySlogan: 'Maneja tus gifs'
+    CompanySlogan: 'Maneja tus gifs',
 
     // Apikeys
-
+     giphyApiKey: 'Fo3GZbaAJiSs4H0VkvUvdXz1JG9ZDZ83',
     // URLs
 };
